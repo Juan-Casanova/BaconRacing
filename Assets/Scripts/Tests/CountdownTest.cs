ab6a0cbec0e341ca3fafe3af193e66677faaa7c7
Paniagua96 <paniagua-11@hotmail.com>
2019-02-25 20:16:44 -0600

    Avance primer prieba unitaria del Countdown

---
+++ Assets/Scripts/Tests/CountdownTest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class CountdownTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void CountdownTestSimplePasses()
+        {
+            //Arrange
+            var countDown = new GameObject().AddComponent<Countdown>();
+            float initialCounter=5.0f;
+            float time=0.0f;
+            bool activateMovement=false;
+
+            //Act
+            countDown.activateMovement(initialCounter, time, activateMovement);
+
+            //Assert
+            Assert.IsFalse(activateMovement);
+
+        }
+
+       
+    }
+}
