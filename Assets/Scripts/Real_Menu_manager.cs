384b1e0e783aa048d97325685cc4e81dee90c0f1
ALD1YO <crespoaldo29@gmail.com>
2019-03-03 02:14:40 -0600

    Se sigue trabajando en la interfaz en general. Se agrega una nueva fuente al proyecto

--- Assets/Scripts/Real_Menu_manager.cs
+++ Assets/Scripts/Real_Menu_manager.cs
@@ -5,22 +5,36 @@ using UnityEngine;
 public class Real_Menu_manager : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject Title_Canvas;
+
     public GameObject Main_Menu_Canvas;
     public GameObject Character_Screen;
     public GameObject Tracks_Screen;
+
+
     public void To_Title_Screen(){
+        Title_Canvas.SetActive(true);
+        Main_Menu_Canvas.SetActive(false);
+        Character_Screen.SetActive(false);
+        Tracks_Screen.SetActive(false);
+    }
+
+    public void To_Main_Menu(){
+        Title_Canvas.SetActive(false);
         Main_Menu_Canvas.SetActive(true);
         Character_Screen.SetActive(false);
         Tracks_Screen.SetActive(false);
     }
 
     public void To_Characters_Screen(){
-         Main_Menu_Canvas.SetActive(false);
+        Title_Canvas.SetActive(false);
+        Main_Menu_Canvas.SetActive(false);
         Character_Screen.SetActive(true);
         Tracks_Screen.SetActive(false);
     }
 
      public void To_Tracks_Screen(){
+        Title_Canvas.SetActive(false);
         Main_Menu_Canvas.SetActive(false);
         Character_Screen.SetActive(false);
         Tracks_Screen.SetActive(true);
