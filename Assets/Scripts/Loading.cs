9f3852bd441fe16d6e949b6bb7cce77fba33a857
Paniagua96 <paniagua-11@hotmail.com>
2019-03-07 18:38:28 -0600

    Ya quedo implementado el minijuego en la pantalla de carga

--- Assets/Scripts/Loading.cs
+++ Assets/Scripts/Loading.cs
@@ -58,7 +58,7 @@ public class Loading : MonoBehaviour
             progressImage.fillAmount = loading.progress;
 
             //Esperamos un frame
-            yield return new WaitForSeconds(3.0f);
+            yield return new WaitForSeconds(5.0f);
 
         }
 
