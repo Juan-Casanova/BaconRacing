fea23057af9cfc29c0a98aeae2d6aef75a74dfb7
Juan-Casanova <casanovajm199621@gmail.com>
2019-03-27 17:09:18 -0600

    Conteo de vueltas

---
+++ Assets/Scripts/Check.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+ using System.Collections;
+ 
+ public class Check : MonoBehaviour {     
+     void  OnTriggerEnter ( Collider other  )
+     {
+         //Is it the Player who enters the collider?
+         if (other.CompareTag("Player")) 
+             return; //If it's not the player dont continue
+         if (transform == ContadorVueltas.spawnA[ContadorVueltas.currentCheckpoint].transform) 
+         {             
+             //Check so we dont exceed our checkpoint quantity
+             if (ContadorVueltas.currentCheckpoint + 1 < ContadorVueltas.spawnA.Length) 
+             {         
+                    ContadorVueltas.currentCheckpoint++;
+                    Debug.Log(ContadorVueltas.spawnA[ContadorVueltas.currentCheckpoint]);              
+             } 
+             else 
+             {
+                 //If we dont have any Checkpoints left, go back to 0
+                 ContadorVueltas.currentLap++;
+                 ContadorVueltas.currentCheckpoint = 0;
+             }
+          } 
+     }
+ 
+ }
+
