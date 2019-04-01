60fddabba886f8100821e6b8dc096ee077c4012b
ald1yo <crespoaldo29@gmail.com>
2019-04-01 10:38:55 -0600

    Se agregan algunos animalitos y un globo "aerostático" con su script de movimiento

---
+++ Assets/Scripts/Movimiento_GloboCerdo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento_GloboCerdo : MonoBehaviour
+{
+
+    public float distancia = 0;
+    public float posicion = 0;
+
+    void Update()
+    {
+         
+
+        transform.position =
+            new Vector3(transform.position.x, Mathf.PingPong(Time.time * 3, distancia) - posicion, transform.position.z);
+    }
+}
