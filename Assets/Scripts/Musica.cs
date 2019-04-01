6d7a7477aee8a4e1e9edc924e1403cffab859c3e
Paniagua96 <paniagua-11@hotmail.com>
2019-03-31 19:47:32 -0600

    Script listo para implementar musica (por error de unity no se pudo implementar)

---
+++ Assets/Scripts/Musica.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Musica : MonoBehaviour
+{
+    public AudioSource audioSource;
+
+    public void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void MusicBackGroundTrack1(AudioSource audioSource)
+    {
+        audioSource.Play();
+        audioSource.loop = true;
+    }
+
+    public void SoundEffect(AudioSource audioSource)
+    {
+        audioSource.Play();
+        audioSource.loop = false;
+
+    }
+}
