d4197a6c6d160ba3a68e5ce54bcaf01ef4c9f6df
Juan-Casanova <casanovajm199621@gmail.com>
2019-04-11 12:07:55 -0500

    Se integra modificacion de sonido de publico a uno mas corto ya funciona el sonido de moneda, lanzamiento y publico

--- Assets/Scripts/ActiveSound.cs
+++ Assets/Scripts/ActiveSound.cs
@@ -6,9 +6,13 @@ public class ActiveSound : MonoBehaviour
 {
     public AudioSource _AudioMoneda;
     public AudioSource _AudioObjeto;
+    public AudioSource _AudioPublico;
+    public AudioSource _AudioChillido;
 
     void OnTriggerEnter(Collider other)
-    { if (other.gameObject.name == "Moneda" || other.gameObject.name == "Moneda (1)"|| other.gameObject.name == "Moneda (2)" || other.gameObject.name == "Moneda (3)") _AudioMoneda.Play(); }
+    { if (other.gameObject.name == "Moneda" || other.gameObject.name == "Moneda (1)"|| other.gameObject.name == "Moneda (2)" || other.gameObject.name == "Moneda (3)") _AudioMoneda.Play();
+        if (other.gameObject.name == "ActivadorPublico") _AudioPublico.Play();
+    }
 
     void Update()
     { if (Input.GetKeyDown(KeyCode.M) || Input.GetKeyDown(KeyCode.E)) _AudioObjeto.Play(); }
