1a4538a94dbb1273e98cb2ebfc0aa09b46b04f3d
Juan-Casanova <casanovajm199621@gmail.com>
2019-04-11 10:37:31 -0500

    Se sube Sonido de publico y se empieza a implementar los demas sonidos

---
+++ Assets/Scripts/ActiveSound.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveSound : MonoBehaviour
+{
+    public AudioSource _AudioMoneda;
+    public AudioSource _AudioObjeto;
+
+    void OnTriggerEnter(Collider other)
+    { if (other.gameObject.name == "Moneda") _AudioMoneda.Play(); }
+
+    void Update()
+    { if (Input.GetKeyDown(KeyCode.M) && Input.GetKeyDown(KeyCode.E)) _AudioObjeto.Play(); }
+}
