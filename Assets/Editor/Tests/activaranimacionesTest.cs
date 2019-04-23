c63d58f8f4b0f1bfb6ca71e3fe247a84cdf6df1b
ald1yo <crespoaldo29@gmail.com>
2019-04-23 03:54:30 -0500

    Se logra implementar un humble object en el script de activaranimaciones

---
+++ Assets/Editor/Tests/activaranimacionesTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+
+    public class activaranimacionesTest
+    {
+        [Test]
+
+        public void When_Numero_Animacion_is_Zero()
+        {
+            //Arrange
+            //ActivaranimacionesEngine activaranimacionesEngine = new ActivaranimacionesEngine();
+        }
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+}
--- Assets/Scripts/activaranimaciones.cs
+++ Assets/Scripts/activaranimaciones.cs
@@ -1,15 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class activaranimaciones : MonoBehaviour
+public class activaranimaciones : MonoBehaviour, Iactivaranimaciones
 {
+    private ActivaranimacionesEngine _activaranimacionesengine;
 
  
-    public Rigidbody Cerdito;
-       public Animator animacioncerdito; //hola animator del cerdito
-   
+    //public Rigidbody Cerdito;
+    public Animator animacioncerdito; // animator del cerdito
 
     /// <summary>
     /// cerdito=0 significa animacion de idle
@@ -21,40 +22,51 @@ public class activaranimaciones : MonoBehaviour
     /// cerdito=6 significa animacion de Item
     /// </summary>
 
+    private void Awake()
+    {
+        _activaranimacionesengine = new ActivaranimacionesEngine(this);
+    }
+
 
     void Update()
     {
-      if (Input.GetKeyDown(KeyCode.M)) //objeto
-        {
-           
-            animacioncerdito.SetInteger("cerdito", 0);
-       }  
-       else if (Input.GetKey(KeyCode.UpArrow) || (Input.GetKey(KeyCode.DownArrow) || (Input.GetKey(KeyCode.LeftArrow) || (Input.GetKey(KeyCode.RightArrow)))))
-        {
-           
-            animacioncerdito.SetInteger("cerdito", 1);
-           
-        }
-        else { { animacioncerdito.SetInteger("cerdito", 0); print("no presionando"); } }
 
-            if (Input.GetKeyDown(KeyCode.Z)) //daño
-        {
-           
-            animacioncerdito.SetInteger("cerdito", 4);
-        }
-        if (Input.GetKeyDown(KeyCode.Space))//salto
-        {
-            
-           
-            animacioncerdito.SetInteger("cerdito", 3);
+        animacioncerdito.SetInteger("cerdito", _activaranimacionesengine.SeleccionAnimacion());
 
-        }
-
-
-
-    }
+    }    
+    
    
 }
 
+public class ActivaranimacionesEngine
+{
+
+    private Iactivaranimaciones _activaranimaciones;
+
+    public ActivaranimacionesEngine(Iactivaranimaciones activaranimaciones)
+    {
+        _activaranimaciones = activaranimaciones;
+    }
+    
+    
+    public int SeleccionAnimacion()
+    {
+        var numeroAnimacion = 0;
+        
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+        {
+            numeroAnimacion = 1;
+        } else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            numeroAnimacion = 3;
+        }
+
+        return numeroAnimacion;
+    }
+}
 
 
+public interface Iactivaranimaciones
+{
+    
+}
