74b18b65b2cd52cc379d395fe0fca9115491ddb6
eseyeyo <eseyeyouni@gmail.com>
2019-05-08 19:55:01 -0500

    Prueba unitaria de Meta. Problema con el contador de vueltas (HUD)

---
+++ Assets/Scripts/IMeta.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IMeta
+{
+    void GameOver(GameObject gameOver);
+
+}
--- Assets/Scripts/Meta.cs
+++ Assets/Scripts/Meta.cs
@@ -5,33 +5,40 @@ using UnityEngine;
 public class Meta : MonoBehaviour
 {
     public int index;
+    MetaEngine _MetaEngine = new MetaEngine();
 
     void OnTriggerEnter(Collider other)
     {
         //if (other.CompareTag("Player1")|| other.CompareTag("Player2")) 
         //CheckCurrentCheckpoint();
-        ContadorVueltas contador = other.GetComponent<ContadorVueltas>();
-        if(contador )
-        if (other.CompareTag("Player") && contador.currentCheckpoint == index-1)
-        {
-            contador.currentLap++;
-            contador.currentCheckpoint = 0;
-            if(contador.currentLap >= 3)
-            {
-                GameManager.instancia.GameOver(other.gameObject);
-            }
-            /*
-            if (checkEngine.currentCheckpoint < checkEngine.lenghtSpawn)
-            {
-                checkEngine.currentCheckpoint++;
-            }
-            else
-            {
-                checkEngine.currentLap++;
-                checkEngine.currentCheckpoint = 0;
-            }
-            */
 
-        }
-    }
+        ContadorVueltas contador = other.GetComponent<ContadorVueltas>();
+        //if (contador)
+            _MetaEngine.Vueltas( other.tag, contador.currentCheckpoint, index, ref contador._currentLap, ref contador.currentCheckpoint, GameManager.instancia, other.gameObject);
+
+
+
+        //if (contador)
+        //    if (other.CompareTag("Player") && contador.currentCheckpoint == index - 1)
+        //    {
+        //        contador.currentLap++;
+        //        contador.currentCheckpoint = 0;
+        //        if (contador.currentLap >= 3)
+        //        {
+        //            GameManager.instancia.GameOver(other.gameObject);
+        //        }
+                //    /*
+                //    if (checkEngine.currentCheckpoint < checkEngine.lenghtSpawn)
+                //    {
+                //        checkEngine.currentCheckpoint++;
+                //    }
+                //    else
+                //    {
+                //        checkEngine.currentLap++;
+                //        checkEngine.currentCheckpoint = 0;
+                //    }
+                //    */
+
+                //}
+            }
 }
