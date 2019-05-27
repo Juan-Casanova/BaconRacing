280813d46debf93b507ce5192bc6001de16cd009
Juan-Casanova <casanovajm199621@gmail.com>
2019-05-26 22:42:11 -0500

    Se agrega script para movimiento de la camara

---
+++ Assets/Scripts/MoverCamaraP1.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverCamaraP1 : MonoBehaviour
+{
+    //private MovimientoCerdoEngine movimientoCerdoEngine;
+    //public Vector3 camara;
+
+    float subir = 0;
+    private bool subiendo = true;
+
+    void Update()
+    {
+        if (Input.GetKey("up"))
+        {
+            MoverCamara();
+        }
+        if (Input.GetKey("up"))
+        {
+            transform.Rotate(0, 0, 0);
+        }
+    }
+
+    void MoverCamara()
+    {
+        if (subiendo)
+        {
+            this.transform.Rotate(subir, 0, 0);
+            subir = subir - .05f;
+            if (subir <= -1.0)
+            {
+                subiendo = false;
+            }
+            Debug.Log(subir);
+        }
+        if (!subiendo)
+        {
+            this.transform.Rotate(subir, 0, 0);
+            subir = subir + .05f;
+            if (subir >= 1.0)
+            {
+                subiendo = true;
+            }
+        }
+
+
+    }
+}
