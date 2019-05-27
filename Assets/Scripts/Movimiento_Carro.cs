dd7d3f55088293e39a898abca884af79c64f78a1
ald1yo <crespoaldo29@gmail.com>
2019-05-26 23:23:14 -0500

    Se agragan los obstaculos a la pista de francia

---
+++ Assets/Scripts/Movimiento_Carro.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento_Carro : MonoBehaviour
+{
+    public int Distancia = 5;
+	public int Posicion = 10;
+	public int Velocidad = 3;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = 
+			new Vector3 ( transform.position.x, transform.position.y, Mathf.PingPong(Time.time*Velocidad,Distancia)-Posicion);
+    }
+}
